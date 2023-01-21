a190f99f66e25519b9d83a8d3738c9495a53ccdd
Joab Almeida <joabalmeida.dev@gmail.com>
2023-01-21 17:15:14 -0300

    emails não podem repetir na BD

--- src/models/LoginModel.js
+++ src/models/LoginModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcryptjs = require('bcryptjs');
 
 const LoginSchema = new mongoose.Schema({
   email: { type: String, required: true },
@@ -18,12 +19,25 @@ class Login {
  async register() {
     this.valida();
     if(this.errors.length > 0) return;
-    try{//sempre usar try catch quando usar await
+
+    await this.userExists();
+
+    if(this.errors.length > 0) return;
+    
+    const salt = bcryptjs.genSaltSync();
+    this.body.password = bcryptjs.hashSync(this.body.password, salt);
+
+    
+    try{
       this.user = await LoginModel.create(this.body);//o this.body já foi validado, criado e preenchido antes
     }catch(e){
       console.log(e);
     }
-   
+  }
+
+  async userExists() {
+    const user = await LoginModel.findOne({ email: this.body.email });//está procurando na BD um email igual ao que está sendo enviado
+    if(user) this.errors.push('Usuário já existe');
   }
 
   valida() {
